eac3902f4b69004dfa3bd1a59efee4ddb63c5449
jooclash491 <42422388+jooclash491@users.noreply.github.com>
2018-08-16 01:19:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,6 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-```
 client.on('message', message => {
     var args = message.content.split(/[ ]+/)
     if(message.content.includes('كسمك')){
