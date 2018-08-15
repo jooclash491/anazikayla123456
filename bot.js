2be37ba8f646bf597bf0afcfb17d391f2ddb3dcb
jooclash491 <42422388+jooclash491@users.noreply.github.com>
2018-08-16 01:31:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,20 +27,5 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-client.on('message', message => {
-    var args = message.content.split(/[ ]+/)
-    if(message.content.includes('كسمك عرص خول متناك هنيكك')){
-        message.delete()
-      message.channel.sendMessage("", {embed: {
-        title: "متشتمش",
-        color: 0x06DF00,
-        description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
-        footer: {
-          text: "By Dr.Joo"
-        }
-      }}).then(msg => {msg.delete(4000)});
-                          }
 
-     
-}); 
 client.login(process.env.BOT_TOKEN);
